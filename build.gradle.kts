1860ebecdc8f9c3deb8325fbb6eecd995729f389
Atomic78p <new88581@gmail.com>
2026-06-16 09:33:27 +0700

    Fix Gradle plugin configuration for stable build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,18 @@
-plugins {
-    id("com.android.application") version "8.7.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
-} 
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "VAN4U-ExternalESP"
+include(":app")
